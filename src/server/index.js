aff16770557c643bab215175cd79d521366d17b9
thomaspeterowen <thomaspeterowen@gmail.com>
2023-05-11 20:12:27 +0200

    added proxy config

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+const { HttpsProxyAgent } = require('https-proxy-agent');
 
 const app = express()
 const port = 3000
@@ -13,11 +14,13 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
+const proxy = 'http://195.233.9.60:8080';
+const proxyAgent = new HttpsProxyAgent(proxy);
 
 // example API call
 app.get('/apod', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`, {agent: proxyAgent})
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
