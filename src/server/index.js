f03fde0cb8b66c5a129178b626b8019f3222e3e0
thomaspeterowen <thomaspeterowen@gmail.com>
2023-06-06 18:58:24 +0200

    removed proxy settings for submission to Udacity

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
-const { HttpsProxyAgent } = require('https-proxy-agent');
+//const { HttpsProxyAgent } = require('https-proxy-agent');
 
 const app = express()
 const port = 3000
@@ -14,13 +14,13 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
-const proxy = 'http://195.233.9.60:8080';
-const proxyAgent = new HttpsProxyAgent(proxy);
+//const proxy = 'http://195.233.9.60:8080';
+//const proxyAgent = new HttpsProxyAgent(proxy);
 
 // NASA pic gallery from specific rover (Curiosity/Opportunity/Spirit)
 app.get('/rovers/:name', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.name}/latest_photos?api_key=${process.env.API_KEY}`, {agent: proxyAgent})
+        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.name}/latest_photos?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
@@ -31,7 +31,7 @@ app.get('/rovers/:name', async (req, res) => {
 // NASA mars rover manifest info
 app.get('/rovers/manifest/:name', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${req.params.name}/?api_key=${process.env.API_KEY}`, {agent: proxyAgent})
+        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${req.params.name}/?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
